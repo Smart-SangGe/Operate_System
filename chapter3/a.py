83c8da52c4023244ba5c6e8a29444e81094f1362
sangge <2251250136@qq.com>
2023-12-21 09:22:32 +0800

    feat: finish a

--- chapter3/a.py
+++ chapter3/a.py
@@ -1,6 +1,22 @@
+import random
+
 N = 30
+
+
+def GenChar(lenth: int, N: int) -> list:
+    # Page Reference List
+    PRlist = []
+    for _ in range(lenth):
+        PRlist.append(random.randrange(N))
+        
+    return PRlist
+
+
 if __name__ == "__main__":
     N = input("input N: ")
-    
-    if N == "":
-        print(111)
+    try:
+        N = int(N)
+    except:
+        N = 30
+    PRlist = GenChar(10, N)
+    print(PRlist)
