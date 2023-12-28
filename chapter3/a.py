ec2c8371eaebb3720baa72faa6569c56447cf23a
sangge <2251250136@qq.com>
2023-12-28 23:32:41 +0800

    fix: add pi as probability

--- chapter3/a.py
+++ chapter3/a.py
@@ -3,10 +3,22 @@ import random
 N = 30
 
 
-def GenChar(lenth: int, N: int) -> list:
+def GenChar(length: int, N: int) -> list:
+    if N <= 0:
+        raise ValueError("N must be bigger than 0")
     page_reference_list = []
-    for _ in range(lenth):
-        page_reference_list.append(random.randrange(N))
+    current_page = random.randrange(N)
+
+    for _ in range(length):
+        pi = random.random()
+        if random.random() < pi:
+            # 以概率pi引用同一页面
+            page_reference_list.append(current_page)
+        else:
+            # 以等概率选择其他页面
+            new_page = random.choice([i for i in range(N) if i != current_page])
+            page_reference_list.append(new_page)
+            current_page = new_page  # 更新当前页面
 
     return page_reference_list
 
