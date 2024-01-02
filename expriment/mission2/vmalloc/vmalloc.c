e0c4f8f8c2f323a9e80932185f8328bb8ada1f93
sangge <2251250136@qq.com>
2024-01-02 17:02:26 +0800

    feat: finish vmalloc

---
+++ expriment/mission2/vmalloc/vmalloc.c
@@ -0,0 +1,50 @@
+#include <linux/module.h>
+#include <linux/vmalloc.h>
+MODULE_LICENSE("GPL");
+unsigned char *vmallocmem1;
+unsigned char *vmallocmem2;
+unsigned char *vmallocmem2;
+static int __init mem_module_init(void)
+{
+    printk("Group 10, Start vmalloc!\n");
+    vmallocmem1 = (unsigned char *)vmalloc(8192, GFP_KERNEL);
+    if (vmallocmem1 != NULL)
+    {
+        printk(KERN_ALERT "Group 10, vmallocmem1 addr = %lx\n", (unsigned long)vmallocmem1);
+    }
+    else
+    {
+        printk("Group 10, Failed to allocate vmallocmem1!\n");
+    }
+    
+    vmallocmem2 = (unsigned char *)vmalloc(1048576, GFP_KERNEL);
+    if (vmallocmem2 != NULL)
+    {
+        printk(KERN_ALERT "Group 10, vmallocmem2 addr = %lx\n", (unsigned long)vmallocmem2);
+    }
+    else
+    {
+        printk("Group 10, Failed to allocate vmallocmem2!\n");
+    }
+
+    vmallocmem3 = (unsigned char *)vmalloc(67108864, GFP_KERNEL);
+    if (vmallocmem3 != NULL)
+    {
+        printk(KERN_ALERT "Group 10, vmallocmem3 addr = %lx\n", (unsigned long)vmallocmem2);
+    }
+    else
+    {
+        printk("Group 10, Failed to allocate vmallocmem3!\n");
+    }
+
+    return 0;
+}
+static void __exit mem_module_exit(void)
+{
+    vfree(vmallocmem1);
+    vfree(vmallocmem2);
+    vfree(vmallocmem3);
+    printk("Group 10, Exit vmalloc!\n");
+}
+module_init(mem_module_init);
+module_exit(mem_module_exit);
