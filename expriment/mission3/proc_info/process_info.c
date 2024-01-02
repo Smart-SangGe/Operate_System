b4bc0168b4cd72185ec8f77227a8b22484c702e3
sangge <2251250136@qq.com>
2024-01-02 18:07:08 +0800

    feat: add output info

--- expriment/mission3/proc_info/process_info.c
+++ expriment/mission3/proc_info/process_info.c
@@ -5,7 +5,7 @@ MODULE_LICENSE("GPL");
 struct task_struct *p;
 static int __init process_info_init(void)
 {
-    printk("Start process_info!\n");
+    printk("Group 10, Start process_info!\n");
     for_each_process(p)
     {
         if (p->state == TASK_RUNNING) // 打印所有正在运行的进程
@@ -16,7 +16,7 @@ static int __init process_info_init(void)
 }
 static void __exit process_info_exit(void)
 {
-    printk("Exit process_info!\n");
+    printk("Group 10, Exit process_info!\n");
 }
 module_init(process_info_init);
 module_exit(process_info_exit);
